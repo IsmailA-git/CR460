386a188345e9e8b7d063f63e7882f2f30f040e89
IsmailA-git <ismail.agouram@etud.polymtl.ca>
2026-03-18 12:22:19 -0400

    Add Azure virtual network

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-cr460-ismail"
   location = "Canada Central"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-cr460-ismail"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
