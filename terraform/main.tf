35ee8a53d8fd9cc91dfdcf6854b9341d1e5ebc03
IsmailA-git <ismail.agouram@etud.polymtl.ca>
2026-03-18 14:09:45 -0400

    Change VM size to Standard_B2s_v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,7 +57,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-cr460-ismail-east"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2s"
+  size                = "Standard_B2s_v2"
   admin_username      = "azureuser"
   admin_password      = "TempPass1234!"
   disable_password_authentication = false
